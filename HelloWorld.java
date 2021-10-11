90292ed1be5e809d535a9f838d936c0e425c2209
TTKXLL002 <76678097+TTKXLL002@users.noreply.github.com>
2021-10-11 23:29:34 +0800

    Update HelloWorld.java
    something

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello Wolrd !");
+        System.out.println("This is my second demo, welcome to join us !");
+    }
+}
